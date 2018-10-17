8680eb7238314517f938660d17184f05869fbb54
Vince <vlin6@stuy.edu>
2018-10-17 13:19:49 -0400

    made app routes

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/edit')
+def edit():
+    return render_tempalte('edit.html')
+
+@app.route('/signup')
+def signup();
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.debug = True  # Set to `False` before release
     app.run()
