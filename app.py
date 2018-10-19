c68a162d5119c98c4bbd64b97aff3a139e1c510e
dkeriazis <dkeriazis@gmail.com>
2018-10-19 13:13:38 -0400

    Began signup

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def index():
 #  def signup():
     #  return render_template('signup.html')
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "GET":
+    if request.method == 'GET':
         if util.accounts.is_logged_in(session):
             return redirect('/')
         else:
-            return render_template("login.html")
+            return render_template('login.html')
 
     # Get values passed via POST
-    username = request.form.get("username")
-    password = request.form.get("password")
+    username = request.form.get('username')
+    password = request.form.get('password')
 
     if util.accounts.auth_user(username, password):
         util.accounts.login_user(session, username)
@@ -38,6 +38,26 @@ def login():
     else:
         flash('Bad username or password')
         return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        if util.accounts.is_logged_in(session):
+            return redirect('/')
+        else:
+            return render_template('signup.html')
+
+    # Get values passed via POST
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm = request.form.get('confirm')
+
+    if password != confirm:
+        flash('Passwords do not match')
+        return render_template('signup.html')
+    else:
+        util.accounts.add_user(username, password)
+        return redirect('/')
 
 if __name__ == '__main__':
     app.debug = True  # Set to `False` before release
