385e271c572ab37532547c30df73428f385fe0f0
Vincent Lin <vlin6@stuy.edu>
2018-10-18 23:26:07 -0400

    added somewhat working paths between login and index pages

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from util.accounts import get_salt
 
 app = Flask(__name__)
 app.secret_key = get_salt()
 
+users = { "username" : "password" }
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if "username" not in session.keys():
+        return redirect(url_for("login"))
+    return render_template("index.html")
 
+'''
 @app.route('/create')
 def create():
     return render_template('create.html')
@@ -27,10 +32,23 @@ def edit():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+'''
 
-@app.route('/login')
+@app.route('/auth', methods = ["GET","POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.form.get("username") not in users.keys():
+        flash("Error: username does not exist")
+        return render_template("login.html")
+    elif users[request.form.get("username")] != request.form.get("password"):
+        flash("Error: password is incorrect") 
+        return render_template("login.html")
+    elif users[request.form.get("username")] == request.form.get("password"):
+        session["username"] = request.form.get("username")
+        return redirect(url_for("index"))
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.debug = True  # Set to `False` before release
