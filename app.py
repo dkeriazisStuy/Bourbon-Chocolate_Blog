a1773c18041a9e476ac461ef7946ce38a7ac74b2
Vince <vlin6@stuy.edu>
2018-10-19 13:18:53 -0400

    added routes to newly added pages

--- app.py
+++ app.py
@@ -8,17 +8,23 @@ app.secret_key = util.accounts.get_salt()
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 #  @app.route('/search')
 #  def search():
-    #  return render_template('search.html')
+#  return render_template('search.html')
 
-#  @app.route('/edit')
-#  def edit():
-    #  return render_tempalte('edit.html')
+@app.route('/edit')
+def edit():
+    if not util.accounts.is_logged_in(session):
+        return redirect('/')
+    return render_template('edit.html')
 
-#  @app.route('/signup')
-#  def signup():
-    #  return render_template('signup.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -38,6 +44,11 @@ def login():
     else:
         flash('Bad username or password')
         return render_template('login.html')
+
+#@app.route('/logout')
+#def logout():
+    
+    
 
 if __name__ == '__main__':
     app.debug = True  # Set to `False` before release
