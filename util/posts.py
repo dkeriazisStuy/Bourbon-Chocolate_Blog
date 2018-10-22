d23eafd92a65683526c9113132bd8f0e47a35b05
dkeriazis <dkeriazis@gmail.com>
2018-10-22 14:25:01 -0400

    Updated posts

--- util/posts.py
+++ util/posts.py
@@ -5,26 +5,15 @@ from string import ascii_letters, digits
 import util.config
 
 
-def connect_db():
-    db = sqlite3.connect(util.config.DB_FILE)
-    c = db.cursor()
-    return db, c
-
-
-def close_db(db):
-    db.commit()
-    db.close()
-
-
 def get_db():
-    db, c = connect_db()
+    db, c = util.config.start_db()
     c.execute("SELECT * FROM posts;")
     print(c.fetchall())
-    close_db(db)
+    util.config.end_db(db)
 
 
 def db_file():
-    db, c = connect_db()
+    db, c = util.config.start_db()
     c.execute("PRAGMA database_list")
     rows = c.fetchall()
     for row in rows:
@@ -33,12 +22,12 @@ def db_file():
 
 # makes posts table in data.db
 def create_table():
-    db, c = connect_db()
+    db, c = util.config.start_db()
     try:
         c.execute("CREATE TABLE posts (id TEXT, title TEXT, content TEXT, author TEXT);")
     except:
         pass
-    close_db(db)
+    util.config.end_db(db)
 
 
 def get_post_id():
@@ -50,27 +39,27 @@ def get_post_id():
 
 # determines if post exists
 def post_exists(id):
-    db, c = connect_db()
+    db, c = util.config.start_db()
     c.execute("SELECT EXISTS(SELECT 1 FROM posts WHERE id = ? LIMIT 1)", (id,))
     result = c.fetchone()[0]
     #print (id + " exists? " + str(result == 1))
-    close_db(db)
+    util.config.end_db(db)
     return result == 1
 
 
 # determines if author exists
 def author_exists(author):
-    db, c = connect_db()
+    db, c = util.config.start_db()
     c.execute("SELECT EXISTS(SELECT 1 FROM posts WHERE author = ? LIMIT 1)", (author,))
     result = c.fetchone()[0]
     #print (id + " exists? " + str(result == 1))
-    close_db(db)
+    util.config.end_db(db)
     return result == 1
 
 
 # adds (creates) a post to posts
 def create_post(id, title, content, author):
-    db, c = connect_db()
+    db, c = util.config.start_db()
     if post_exists(id): # if the post exists already
         print(":P post *" + id + "* exists try again")
         pass
@@ -78,12 +67,12 @@ def create_post(id, title, content, author):
         params = (id, title, content, author)
         c.execute("INSERT INTO posts VALUES (?,?,?,?)", params)
         print ("create_post: " + str(id) + "\t" + title + "\t" + content + "\t" + author) # see that it runs, comment out later
-    close_db(db)
+    util.config.end_db(db)
 
 
 # gets a post from posts
 def get_post(id):
-    db, c = connect_db()
+    db, c = util.config.start_db()
     if post_exists(id): # if the post exists, get it
         c.execute("SELECT * FROM posts WHERE id = ?;", (id,))
         rows = c.fetchall()
@@ -96,12 +85,12 @@ def get_post(id):
         # c.fetchall() should only return 1 post
     else: # if the post doesn't exist, do nothing
         pass
-    close_db(db)
+    util.config.end_db(db)
 
 
 # gets all of an author's posts from posts
 def get_author_posts(author):
-    db, c = connect_db()
+    db, c = util.config.start_db()
     if author_exists(author): # if the author exists, get all of their posts
         c.execute("SELECT * FROM posts WHERE author = ?;", (author,))
         print ("get_post: " + author + "\t" + str(c.fetchall())) # see that it runs, comment out later
@@ -109,32 +98,35 @@ def get_author_posts(author):
     else: # if the author doesn't exist
         print("this ain't it chief")
         pass
-    close_db(db)
+    util.config.end_db(db)
 
 
 # deletes a post from posts
-def delete_post(id):
-    db, c = connect_db()
-    if post_exists(id): # if the post exists, delete it
-        c.execute("DELETE FROM posts WHERE title = ?;", (id,))
-        print("delete_post: " + id) # see that it runs, comment out later
+def delete_post(post):
+    db, c = util.config.start_db()
+    if post_exists(post): # if the post exists, delete it
+        c.execute("DELETE FROM posts WHERE id = ?", (post,))
+        print("delete_post: " + post) # see that it runs, comment out later
     else: # if the post doesn't exist
-        print(":P post " + id + " doesn't exist")
+        print(":P post " + post + " doesn't exist")
         pass
-    close_db(db)
+    util.config.end_db(db)
 
 
 # edits a post from posts
-def edit_post(id, title, content):
-    db, c = connect_db()
-    if post_exists(id): # if the post exists, edit it
-        params = (title, content, id)
-        c.execute("UPDATE posts SET title = (?), content = (?)  WHERE id = (?);", params)
-        print("edited: " + id + "\t" + title + "\t" + content) # see that it runs, comment out later
+def edit_post(post, title, content):
+    db, c = util.config.start_db()
+    if post_exists(post): # if the post exists, edit it
+        c.execute(
+            "UPDATE posts SET title = (?), content = (?)  WHERE id = (?)",
+            (title, content, post)
+        )
+        # see that it runs, comment out later
+        #  print("edited: " + post + "\t" + title + "\t" + content)
     else: # if the post doesn't exist
-        print(":P post " + id + " doesn't exist")
+        #  print(":P post " + post + " doesn't exist")
         pass
-    close_db(db)
+    util.config.end_db(db)
 
 
 def render_post(content):
